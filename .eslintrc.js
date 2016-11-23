369eaa954869437ff0557bb0a07472cf4d834f18
Sadaf Shahsahebi <sadaf@meetup.com>
2016-11-23 12:44:30 -0500

    fix eslintrc fules

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,6 +48,18 @@ const config = {
 				"ClassDeclaration": true
 			}
 		}],
+		"react/jsx-no-undef": 0,
+		"react/jsx-uses-react": 1,
+		"react/jsx-uses-vars": 1,
+		"react/no-deprecated": 2,
+		"react/no-string-refs": 1,
+		"react/react-in-jsx-scope": 2,
+		"react/jsx-wrap-multilines": 2,
+		"react/jsx-boolean-value": 2,
+		"react/jsx-equals-spacing": [2, "never"],
+		"react/jsx-no-duplicate-props": 2,
+		"react/jsx-pascal-case": 2,
+		"react/jsx-uses-react": 2,
 		"semi": 2,
 		"space-in-parens": [2, "never"],
 		"spaced-comment": [2, "always"],
