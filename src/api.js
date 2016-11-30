2f7ae2631bce91838468fadc96e062e75919d569
Lori Hutchek <Lori.Hutchek@akqa.com>
2016-11-30 18:40:48 -0500

    updating comments mock

--- src/api.js
+++ src/api.js
@@ -44,6 +44,30 @@ export const MOCK_GROUP = {
 	}],
 };
 
+export const MOCK_CHAPTER_MEMBER = {
+	id:82159492,
+	name:"Grayson Cheese",
+	status:"active",
+	joined:1361806241000,
+	city:"new york",
+	country:"us",
+	lat:40,
+	lon:-72,
+
+	photo: {
+		photo_link: 'http://placekitten.com/g/400/400',
+	},
+	group_profile: {  
+	  status:"active",
+	  visited:1396285003000,
+	  created:1395768492000,
+	  updated:1478880527000,
+	  group: MOCK_GROUP,
+	  intro: "cheese is the best!",
+	}
+};
+
+
 const oneMonthAgo = new Date();
 oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
 const oneMonthFuture = new Date();
@@ -111,25 +135,123 @@ export const MOCK_GROUP_COMMUNICATION = {
 };
 
 
-export const MOCK_GROUP_COMMENTS = {
-	"id":1342,
-	"description":"Some Mock Conversation",
-	"hashtag":"#ideas",
-	"group":MOCK_GROUP,
-	"creator":MOCK_MEMBER,
-	"num_comments":63,
-	"deleted":false,
-	"visible":true,
-	"created":1477520127,
-	"utc_offset":-18000000,
-	"updated":1477520127,
-	"self":{
-		"followed":true,
-		"actions":[
-			"delete",
-			"comment",
-			"follow",
-			"flag_spam"
-		]
-	}
+export const MOCK_GROUP_COMMENTS = {  
+   "0":{  
+      "id":1234,
+      "comment":"blue!",
+      "created":1479838415,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1479838415,
+      "updated":1479838415,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "delete"
+         ]
+      }
+   },
+   "1":{  
+      "id":12345,
+      "comment":"test 40",
+      "created":1478879940,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1478879940,
+      "updated":1478879940,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "delete",
+            "flag_spam"
+         ]
+      }
+   },
+   "2":{  
+      "id":21234,
+      "comment":"test 1",
+      "created":1478879939,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1478879939,
+      "updated":1478879939,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "flag_spam"
+         ]
+      }
+   },
+   "3":{  
+      "id":23423,
+      "comment":"test 2",
+      "created":1478879939,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1478879939,
+      "updated":1478879939,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "delete",
+            "flag_spam"
+         ]
+      }
+   },
+   "4":{  
+      "id":234,
+      "comment":"test 3",
+      "created":1478879939,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1478879939,
+      "updated":1478879939,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "delete",
+            "flag_spam"
+         ]
+      }
+   },
+   "5":{  
+      "id":23423,
+      "comment":"test 4",
+      "created":1478879939,
+      "like_count":0,
+      "member":MOCK_CHAPTER_MEMBER,
+      "deleted":false,
+      "visible":true,
+      "created":1478879939,
+      "updated":1478879939,
+      "utc_offset":-18000000,
+      "self":{  
+         "liked":false,
+         "actions":[  
+            "like",
+            "delete",
+            "flag_spam"
+         ]
+      }
+   }
 };
