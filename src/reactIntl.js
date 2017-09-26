1be7767c01e20cacd5d84f26ef6369d9eb554013
Lori Hutchek <Lori.Hutchek@akqa.com>
2017-09-26 17:38:44 -0400

    fixing returns

--- src/reactIntl.js
+++ src/reactIntl.js
@@ -1,9 +1,9 @@
 export const intl = {
 	formatMessage: jest.fn(trn => trn.defaultMessage || 'formatted message'),
 	formatDate: jest.fn(date => date.toString() || 'formatted date'),
-	formatTime: jest.fn(time => time || 'formatted time'),
+	formatTime: jest.fn(time => time.toString() || 'formatted time'),
 	formatRelative: jest.fn(date => date.toString() || 'formatted relative time'),
-	formatNumber: jest.fn(num => num || 123),
+	formatNumber: jest.fn(num => String(num) || '123'),
 	formatPlural: jest.fn(str => str || 'formatted plural string'),
 	formatHTMLMessage: jest.fn(trn => trn.defaultMessage || 'formatted HTML message'),
 	now: jest.fn(date => 12356789),
